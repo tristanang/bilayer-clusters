629c66a280e0ed49311607a47b0767facd0ac63a
tristanang <tangtzeheng@wesleyan.edu>
2018-01-07 13:50:31 -0500

    added trajectory compression script

--- analysis/compress.py
+++ analysis/compress.py
@@ -1,17 +1,19 @@
 import trajIO
 import sys
-import json
 import constants as c
 
-def spitjson(trajFileName, topology, Nconf):
-	Nchol = trajIO.cholConc
-	N,L,com_lipids,com_chol = trajIO.processTrajCOM(trajFileName,Nchol,c.NDIM,Nconf)
-	#json dump
+def spitnumpy(trajFileName, topology, Nconf):
+    outfile = "comTraj"
+    Nchol = trajIO.cholConc(topology)
+    N,L,com_lipids,com_chol = trajIO.processTrajCOM(trajFileName,Nchol,c.NDIM,Nconf)
+    
+    trajIO.np.savez(outfile,L=L,com_lipids=com_lipids,com_chol=com_chol)
 
 if __name__ == "__main__":
-	trajFileName = sys.argv[1]
-	Nconf = int(sys.argv[2])
-	nlog = int(sys.argv[3])
-	topology = sys.argv[4]
+    trajFileName = sys.argv[1]
+    Nconf = int(sys.argv[2])
+    topology = sys.argv[3]
 
-	#print json to file
+    spitnumpy(trajFileName, topology, Nconf)
+
+    #print json to file
