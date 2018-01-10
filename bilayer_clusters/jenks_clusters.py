81bf11eb5edfcfcec44dd1825615283cbc0b5550
tristanang <tangtzeheng@wesleyan.edu>
2018-01-10 17:55:13 -0500

    fixed bugs in jenks_clusters clusters_base. Now working on clusters with lipids

--- bilayer_clusters/jenks_clusters.py
+++ bilayer_clusters/jenks_clusters.py
@@ -1,48 +1,25 @@
-from jenks import jenks
-from bisect import bisect_left
+from bilayer_clusters import jenks
 import numpy as np
-
-def index(a, x):
-    'Locate the leftmost value exactly equal to x'
-    i = bisect_left(a, x)
-    if i != len(a) and a[i] == x:
-        return i
-    raise ValueError
-
-def binary_search(arr,x,lo=0,hi=None):
-    if not hi:
-        hi = len(arr)
-
-    found = False
-
-    while lo<=hi and not found:
-        mid = (lo + hi)//2
-        if arr[mid] == x:
-            return mid
-        elif arr[mid]>x:
-            hi = mid-1
-        elif arr[mid]<x:
-            lo = mid+1
-        else:
-            raise ValueError
-
-    return ValueError
 
 def clusters_base(arr,k): #array is pre-sorted
-    breaks = jenks(arr,k)
-    print(breaks)
-    clusters = []
+    breaks = jenks.jenks(arr,k)
+    clusters = [0 for i in range(k)]
 
-    breaks = list(map(lambda x : binary_search(breaks,x),breaks))
+    i_start = 0
+    i = 0
 
-    return(breaks)
+    for index in breaks[1:-1]:
+        i_end = index
+        clusters[i] = arr[i_start:i_end+1]
+        i += 1
+        i_start = i_end+1
 
-import numpy as np
-arr = [1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,100.0]
-arr.sort()
-print(arr)
-print(clusters_base(arr,3))
+    clusters[i] = arr[i_start:]
 
+    return clusters
+
+def clusters(lipids,k,t):
+    return
 
 def clusterForm(group,interval,endblock):
     clusters = []
--- test/test_jenks_clusters.py
+++ test/test_jenks_clusters.py
@@ -2,17 +2,32 @@ import json
 from bilayer_clusters import jenks_clusters
 
 def test_json():
-    data = json.load(open('test.json'))
-    clusters = jenks_clusters.clusters_base(data,5)
-    breaks = list(map(lambda x : breaks[x],breaks))
+    data = json.load(open('jenks.json'))
+    data.sort()
 
-    assert [round(v, 6) for v in breaks] == [0.002811,
-                                             2.093548,
-                                             4.205495,
-                                             6.178148,
-                                             8.091759,
-                                             9.997983]
+    cluster1 = data[:442+1]
+    cluster2 = data[443:858+1]
+    cluster3 = data[859:1246+1]
+    cluster4 = data[1247:1604+1]
+    cluster5 = data[1605:]
+
+    cluster = [cluster1,cluster2,cluster3,cluster4,cluster5]
+    
+    clusterlen = list(map(len,cluster))
+    assert len(data) == sum(clusterlen)
+    #this proves our manual cluster is correct.
+
+    clusterfunc = jenks_clusters.clusters_base(data,5)
+    clusterfunclen = list(map(len,clusterfunc))
+
+    assert clusterfunclen == clusterlen
+    
+    assert clusterfunc == cluster
+
+    return
 
 if __name__ == '__main__':
-    test_json
+    test_json()
     print("passed")
+
+#[0, 442, 858, 1246, 1604, 1999]
