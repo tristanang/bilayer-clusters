0879fb68417b3b9dbd02cf1b59f01c9ae5032cd8
tristanang <tangtzeheng@wesleyan.edu>
2018-01-10 03:14:52 -0500

    fixed jenks function to use indexing and modified its respective test

---
+++ bilayer_clusters/gvf.py
@@ -0,0 +1,33 @@
+from jenks import jenks
+def goodness_of_variance_fit(array, classes):
+    # get the break points
+    classes = jenks(array, classes)
+
+    # do the actual classification
+    classified = np.array([classify(i, classes) for i in array])
+
+    # max value of zones
+    maxz = max(classified)
+
+    # nested list of zone indices
+    zone_indices = [[idx for idx, val in enumerate(classified) if zone + 1 == val] for zone in range(maxz)]
+
+    # sum of squared deviations from array mean
+    sdam = np.sum((array - array.mean()) ** 2)
+
+    # sorted polygon stats
+    array_sort = [np.array([array[index] for index in zone]) for zone in zone_indices]
+
+    # sum of squared deviations of class means
+    sdcm = sum([np.sum((classified - classified.mean()) ** 2) for classified in array_sort])
+
+    # goodness of variance fit
+    gvf = (sdam - sdcm) / sdam
+
+    return gvf
+
+def classify(value, breaks):
+    for i in range(1, len(breaks)):
+        if value < breaks[i]:
+            return i
+    return len(breaks) - 1
---
+++ bilayer_clusters/jenks_clusters.py
@@ -0,0 +1,12 @@
+import numpy as np
+#cimport numpy as np
+
+def jenks_trial(arr, k): #k refers to number of clusters
+    arr.sort()
+    
+    output = []
+    SDAM = np.sum((arr - np.mean(arr))**2)
+
+def jenks_clusters(lipids, k, t):
+
+    return
